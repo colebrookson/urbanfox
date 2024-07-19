d163c0a55c3c809e42c1bf935f5cf5987ae2c538
colebrookson <cole.brookson@gmail.com>
2024-07-19 15:56:10 -0400

    alternate way of doing this in terra

--- src/R/00_data_read_krig.r
+++ src/R/00_data_read_krig.r
@@ -10,17 +10,19 @@ library(stringr)
 library(ggplot2)
 library(gstat)
 library(here)
+library(raster)
+library(terra)
 
 # read in data ===========
 
 # REMEMBER TO CHANGE THE PATH FOR YOUR CASE
 germany_sf <- sf::st_read(here::here("./data/raw/geo-data/")) %>%
-    st_as_sf()
+  st_as_sf()
 germany_code_matches <- readr::read_csv(here::here(
-    "./data/raw/zuordnung_plz_ort.csv"
+  "./data/raw/zuordnung_plz_ort.csv"
 ))
 pharos_data <- readr::read_csv(here::here(
-    "./data/raw/pharos_data.csv"
+  "./data/raw/pharos_data.csv"
 ))
 
 # clean up data =====
@@ -30,15 +32,15 @@ pharos_data <- readr::read_csv(here::here(
 # recognizable english
 names(germany_sf)
 germany_sf <- germany_sf %>% dplyr::rename(
-    "zip_code" = "plz", "population" = "einwohner", "km_sq" = "qkm"
+  "zip_code" = "plz", "population" = "einwohner", "km_sq" = "qkm"
 )
 names(germany_sf) # see what the new names look like
 
 # do the same re-naming to the code matches dataset
 names(germany_code_matches)
 germany_code_matches <- germany_code_matches %>% dplyr::rename(
-    "zip_code" = "plz", "location" = "ort", "district" = "landkreis",
-    "federal_state" = "bundesland"
+  "zip_code" = "plz", "location" = "ort", "district" = "landkreis",
+  "federal_state" = "bundesland"
 )
 
 # NOTE
@@ -53,33 +55,33 @@ germany_code_matches <- germany_code_matches %>% dplyr::rename(
 # filter the `germany_code_matches` data to only the postal codes that are
 # in berlin
 berlin_codes <- germany_code_matches[which(
-    germany_code_matches$location == "Berlin"
+  germany_code_matches$location == "Berlin"
 ), ]
 berlin_sf <- germany_sf[which(germany_sf$zip_code %in% berlin_codes$zip_code), ]
 
 # plot with a population fill just for fun
 ggplot2::ggplot(berlin_sf) +
-    geom_sf() +
-    theme_void()
+  geom_sf() +
+  theme_void()
 
 # NOTE
 #' Now let's sample this whole area with points, so we can then perform the
 #' kirgging to the points we have data for
 grid_sample <- sf::st_sample(
-    sf::st_as_sfc(berlin_sf),
-    # the size is really large to make a fine grid - you can change this
-    size = 10000, type = "regular"
+  sf::st_as_sfc(berlin_sf),
+  # the size is really large to make a fine grid - you can change this
+  size = 10000, type = "regular"
 ) %>%
-    sf::st_as_sf()
+  sf::st_as_sf()
 # this is important to make sure our georefs are the same
 sf::st_crs(grid_sample) <- 4326
 
 # plot these two to show how it looks
 ggplot2::ggplot() +
-    geom_sf(data = berlin_sf, fill = "grey30") +
-    geom_sf(data = grid_sample, colour = "red", size = 4) +
-    theme_void() +
-    coord_sf()
+  geom_sf(data = berlin_sf, fill = "grey30") +
+  geom_sf(data = grid_sample, colour = "red", size = 4) +
+  theme_void() +
+  coord_sf()
 
 # NOTE
 # now we need to figure out how to make the points about berlin foxes talk to
@@ -93,17 +95,17 @@ sf::st_crs(pharos_sf) <- 4326
 
 # make sure we can plot them all on the same geo-location
 ggplot2::ggplot() +
-    geom_sf(data = berlin_sf, aes(fill = population), alpha = 0.3) +
-    geom_sf(data = grid_sample, colour = "red", size = 2) + # sampled points
-    geom_sf(data = pharos_sf, colour = "purple", size = 3) + # foxes!
-    theme_void() +
-    coord_sf()
+  geom_sf(data = berlin_sf, aes(fill = population), alpha = 0.3) +
+  geom_sf(data = grid_sample, colour = "red", size = 2) + # sampled points
+  geom_sf(data = pharos_sf, colour = "purple", size = 3) + # foxes!
+  theme_void() +
+  coord_sf()
 
 # for mapping purposes, it would be nice to have a single polygon of berlin
 # that doesn't include the postal codes but is just one big thing
 berlin_poly <- sf::st_union(berlin_sf)
 ggplot2::ggplot() +
-    geom_sf(data = berlin_poly) +# this is what we want
+  geom_sf(data = berlin_poly) + # this is what we want
   theme_void()
 
 
@@ -111,13 +113,13 @@ ggplot2::ggplot() +
 #' now we want to be able to put the various data we have into a format that
 #' will let us do indicator kriging on it -- binary logical data
 pharos_sf <- pharos_sf[which(
-    pharos_sf$detection_outcome != "inconclusive"
+  pharos_sf$detection_outcome != "inconclusive"
 ), ]
 pharos_sf$detection_outcome[which(
-    pharos_sf$detection_outcome == "positive"
+  pharos_sf$detection_outcome == "positive"
 )] <- TRUE
 pharos_sf$detection_outcome[which(
-    pharos_sf$detection_outcome == "negative"
+  pharos_sf$detection_outcome == "negative"
 )] <- FALSE
 pharos_sf$detection_outcome <- as.logical(pharos_sf$detection_outcome)
 sf::st_crs(pharos_sf) <- 4326
@@ -155,14 +157,14 @@ vgm1 <- gstat::vgm(
 # save the plot of a particular parameter combination
 png(filename = here::here("./figs/varg-vgm-020-1-001-Exp.png")) # opens the png
 plot(varg, vgm1) # the thing you're actually saving
-dev.off() # turns off the "opener" so you can do other things - you need one of 
-# these every time you "open" with png() 
+dev.off() # turns off the "opener" so you can do other things - you need one of
+# these every time you "open" with png()
 
 vgm2 <- gstat::vgm(
-  psill = 0.2, 
-  range = 1, 
-  nugget = 0.01, 
-  model = "Gau" 
+  psill = 0.2,
+  range = 1,
+  nugget = 0.01,
+  model = "Gau"
 )
 # save the plot of a particular parameter combination
 png(filename = here::here("./figs/varg-vgm-020-1-001-Gau.png")) # opens the png
@@ -174,49 +176,74 @@ fit_varg <- gstat::fit.variogram(varg, vgm0)
 
 # krigging ====
 krig <- gstat::krige(
-    detection_outcome ~ 1,
-    locations = pharos_sf,
-    newdata = grid_sample,
-    model = fit_varg,
-    nmax = 5
+  detection_outcome ~ 1,
+  locations = pharos_sf,
+  newdata = grid_sample,
+  model = fit_varg,
+  nmax = 5
 )
+
 plot(krig["var1.pred"])
 
 krig_and_foxes <- ggplot2::ggplot() +
-    geom_sf(data = berlin_poly, alpha = 0.3) +
-    # geom_sf(data = grid_sample, colour = "red", size = 2) + # sampled points
-    geom_sf(data = krig, aes(fill = var1.pred), shape = 21, size = 3) +
-    geom_sf(data = pharos_sf, aes(colour = detection_outcome), size = 2) + # foxes
-    scale_fill_viridis_c("probability", na.value = "white") +
-    scale_colour_manual("test outcome", values = c("#8a56b8", "#d5363d")) +
-    theme_void() +
-    coord_sf()
+  geom_sf(data = berlin_poly, alpha = 0.3) +
+  # geom_sf(data = grid_sample, colour = "red", size = 2) + # sampled points
+  geom_sf(data = krig, aes(fill = var1.pred), shape = 21, size = 3) +
+  geom_sf(data = pharos_sf, aes(colour = detection_outcome), size = 2) + # foxes
+  scale_fill_viridis_c("probability", na.value = "white") +
+  scale_colour_manual("test outcome", values = c("#8a56b8", "#d5363d")) +
+  theme_void() +
+  coord_sf()
 ggplot2::ggsave(
   filename = here::here("./figs/krigging-with-foxes-outcomes.png"),
   plot = krig_and_foxes,
-  height = 6, 
+  height = 6,
   width = 8,
-  bg = "white" # change if you want transparent background  
+  bg = "white" # change if you want transparent background
 )
 
 plot(krig_and_foxes)
 
-##save as raster ====
-library(raster)
-library(terra)
+## save as raster ====
 
-template = rast(vect(krig),res=0.005)
-poly_raster <- rasterize(vect(krig), template)
-plot(poly_raster,col="red")
+raster_template <- terra::rast(terra::vect(berlin_poly), res = 0.0005)
+raster_area <- terra::rasterize(terra::vect(berlin_poly), raster_template)
+terra::plot(raster_area)
 
-krig_raster <- as.raster(poly_raster)
+
+krig_raster <- krig %>%
+  terra::vect() %>%
+  terra::rast()
+
+terra::values(krig_raster) <- krig$var1.pred
+terra::plot(krig_raster)
+
+# Convert points to sp (assumes that the sf object is called example_points)
+krig_points <- as(krig, "Spatial")
+
+# Generate empty raster layer and rasterize points
+krig_raster <- raster::raster(
+  crs = crs(krig_points),
+  # vals = 0,
+  resolution = 0.005,
+  ext = raster::extent(krig_points)
+) %>%
+  raster::rasterize(krig_points, .)
+raster::plot(krig_raster, "var1.pred")
+
+
+template <- terra::rast(terra::vect(krig), res = 0.005)
+poly_raster <- raster::rasterize(terra::vect(krig), template)
+plot(poly_raster)
+
+krig_raster <- terra::as.raster(poly_raster)
 class(krig_raster)
 
-writeRaster(krig_raster, 
-            "./figs/krig_raster.png", 
-            format="Gtiff", 
-            overwrite = TRUE) 
-?writeRaster
+terra::writeRaster(krig_raster,
+  here::here("./clean/krig_raster.gtif"),
+  overwrite = TRUE
+)
+
 
 ## alternative krig ====
 ### vgm1 ====
@@ -243,9 +270,9 @@ krig_and_foxes <- ggplot2::ggplot() +
 ggplot2::ggsave(
   filename = here::here("./figs/krigging-with-foxes-outcomes-1.png"),
   plot = krig_and_foxes,
-  height = 6, 
+  height = 6,
   width = 8,
-  bg = "white" # change if you want transparent background  
+  bg = "white" # change if you want transparent background
 )
 
 ### vgm2 ====
@@ -272,14 +299,14 @@ krig_and_foxes <- ggplot2::ggplot() +
 ggplot2::ggsave(
   filename = here::here("./figs/krigging-with-foxes-outcomes-2.png"),
   plot = krig_and_foxes,
-  height = 6, 
+  height = 6,
   width = 8,
-  bg = "white" # change if you want transparent background  
+  bg = "white" # change if you want transparent background
 )
 
 
 # aesthetic ====
-#trying to make it prettier
+# trying to make it prettier
 krig_and_foxes <- ggplot2::ggplot() +
   geom_sf(data = berlin_poly, alpha = 0.3) +
   geom_sf(data = krig, aes(fill = var1.pred), shape = 21, size = 3, colour = NA, stroke = 0) + # same color outline and fill
@@ -289,6 +316,3 @@ krig_and_foxes <- ggplot2::ggplot() +
   theme_void() +
   coord_sf()
 plot(krig_and_foxes)
-
-
-
