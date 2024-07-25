21943c07c9d040c946100327c3b34839d57f8379
arianebussieres <abussieresfournel@gmail.com>
2024-07-25 13:10:19 -0400

    reading in tif

--- src/julia/heatmap_testing.jl
+++ src/julia/heatmap_testing.jl
@@ -3,6 +3,7 @@ using NeutralLandscapes
 using CairoMakie
 using PyPlot
 
+#example
 measurements = rand(MidpointDisplacement(), (200, 200)) .* 100
 heatmap(measurements)
 
@@ -25,3 +26,23 @@ function plotPointsHeatmap()
     CairoMakie.scatter!(getindex.(locations, 1), getindex.(locations, 2))
 end 
 plotPointsHeatmap()
+
+# reading in the raster (outline of berlin)
+using Pkg
+using GMT
+file = gmtread("C:\\Users\\abuss\\github\\urbanfox\\data\\clean\\krig_raster.tif")
+
+# trying to figure out why I can't entropize it
+map_entropy = convert.(Float64, file)
+
+#entropize berlin map
+heatmap(map_entropy) #working
+
+map_e = entropize(map_entropy) 
+heatmap(map_e) #not working
+
+locations =
+    map_entropy |> 
+    entropize |> 
+    seed(BalancedAcceptance(; numpoints = 100)) |> 
+    first
