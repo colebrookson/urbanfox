7b8a1603cb4289f9be2a28c77d471683dbfbf44f
arianebussieres <abussieresfournel@gmail.com>
2024-07-26 12:36:10 -0400

    locations needed for the kriging :)

--- src/R/01_explore_data.R
+++ src/R/01_explore_data.R
@@ -1,6 +1,62 @@
 # Explore the data
 
+library(sp)
+library(gstat)
+suppressPackageStartupMessages({
+  library(dplyr) # for "glimpse"
+  library(ggplot2)
+  library(scales) # for "comma"
+  library(magrittr)
+})
+
+coord50_1 <- readr::read_table(here::here(
+  "./data/clean/coord50_1.txt"), col_names = FALSE)
+coord50_2 <- readr::read_table(here::here(
+  "./data/clean/coord50_2.txt"), col_names = FALSE)
+coord50_3 <- readr::read_table(here::here(
+  "./data/clean/coord50_3.txt"), col_names = FALSE)
+coord50_4 <- readr::read_table(here::here(
+  "./data/clean/coord50_4.txt"), col_names = FALSE)
+coord50_5 <- readr::read_table(here::here(
+  "./data/clean/coord50_5.txt"), col_names = FALSE)
+coord50_6 <- readr::read_table(here::here(
+  "./data/clean/coord50_6.txt"), col_names = FALSE)
+coord50_7 <- readr::read_table(here::here(
+  "./data/clean/coord50_7.txt"), col_names = FALSE)
+coord50_8 <- readr::read_table(here::here(
+  "./data/clean/coord50_8.txt"), col_names = FALSE)
+coord50_9 <- readr::read_table(here::here(
+  "./data/clean/coord50_9.txt"), col_names = FALSE)
+coord50_10 <- readr::read_table(here::here(
+  "./data/clean/coord50_10.txt"), col_names = FALSE)
+coord10 <- readr::read_table(here::here(
+  "./data/clean/coord10.txt"), col_names = FALSE)
+coord100 <- readr::read_table(here::here(
+  "./data/clean/coord100.txt"), col_names = FALSE)
+coord200 <- readr::read_table(here::here(
+  "./data/clean/coord200.txt"), col_names = FALSE)
+coord300 <- readr::read_table(here::here(
+  "./data/clean/coord300.txt"), col_names = FALSE)
+coord500 <- readr::read_table(here::here(
+  "./data/clean/coord500.txt"), col_names = FALSE)
+#coord1000 <- readr::read_table(here::here("./data/clean/coord1000.txt"), col_names = FALSE)
+#coord2000 <- readr::read_table(here::here("./data/clean/coord2000.txt"), col_names = FALSE)
+#coord5000 <- readr::read_table(here::here("./data/clean/coord5000.txt"), col_names = FALSE)
+#coord10000 <- readr::read_table(here::here("./data/clean/coord10000.txt"), col_names = FALSE)
+#coord25000 <- readr::read_table(here::here("./data/clean/coord25000.txt"), col_names = FALSE)
+
+#prep de data
+class(coord50_1)
+coordinates(coord50_1) <- ~ X1 + X2
+class(coord50_1)
+
+#variogram
+lzn.vgm <- variogram(log(zinc)~1, meuse) # calculates sample variogram values 
+lzn.fit <- fit.variogram(lzn.vgm, model=vgm(1, "Sph", 900, 1)) # fit model
+
 # Krig 50 points ====
+# On veut savoir si la randomness a un effet sur la qualité de l'échantillonnage
+# Est-il similaire au krig original ? Plus ou moins précis ? Uniforme entre eux ?
 krig50 <- gstat::krige(
   detection_outcome ~ 1,
   locations = pharos_50,
@@ -21,75 +77,73 @@ krig_50pts <- ggplot2::ggplot() +
 
 ## map 1 ====
 plot(krig_50pts)
-
+## map 2 ====
+plot(krig_50pts)
+## map 3 ====
+plot(krig_50pts)
+## map 4 ====
+plot(krig_50pts)
+## map 5 ====
+plot(krig_50pts)
+## map 6 ====
+plot(krig_50pts)
+## map 7 ====
+plot(krig_50pts)
+## map 8 ====
+plot(krig_50pts)
+## map 9 ====
+plot(krig_50pts)
+## map 10 ====
+plot(krig_50pts)
 
 # Krig 10 points ====
-krig10 <- gstat::krige(
-  detection_outcome ~ 1,
-  locations = pharos_10,
-  newdata = grid_sample,
-  model = fit_varg10,
-  nmax = 5
-)
-plot(krig10["var1.pred"])
+# On veut savoir si le nombre de points (sous-échantillon) a un effet sur la 
+# qualité du krigeage. Est-il similaire au krig original ? Y a-t-il un plateau
+# ou un nombre de ptns ou il n'y a pas de différéence significative ?
 
-krig_10pts <- ggplot2::ggplot() +
-  geom_sf(data = berlin_poly, alpha = 0.3) +
-  geom_sf(data = krig10, aes(fill = var1.pred), shape = 21, size = 3) +
-  geom_sf(data = pharos_10, aes(colour = detection_outcome), size = 2) + # foxes
-  scale_fill_viridis_c("probability", na.value = "white") +
-  scale_colour_manual("test outcome", values = c("#8a56b8", "#d5363d")) +
-  theme_void() +
-  coord_sf()
-
-## map 2 ====
+## map 11 ====
 plot(krig_10pts)
 
 # Krig 100 points ====
-krig100 <- gstat::krige(
-  detection_outcome ~ 1,
-  locations = pharos_100,
-  newdata = grid_sample,
-  model = fit_varg100,
-  nmax = 5
-)
-plot(krig100["var1.pred"])
 
-krig_100pts <- ggplot2::ggplot() +
-  geom_sf(data = berlin_poly, alpha = 0.3) +
-  geom_sf(data = krig100, aes(fill = var1.pred), shape = 21, size = 3) +
-  geom_sf(data = pharos_100, aes(colour = detection_outcome), size = 2) + # foxes
-  scale_fill_viridis_c("probability", na.value = "white") +
-  scale_colour_manual("test outcome", values = c("#8a56b8", "#d5363d")) +
-  theme_void() +
-  coord_sf()
-
-## map 3 ====
+## map 12 ====
 plot(krig_100pts)
 
-# Krig 500 points ====
-krig500 <- gstat::krige(
-  detection_outcome ~ 1,
-  locations = pharos_500,
-  newdata = grid_sample,
-  model = fit_varg500,
-  nmax = 5
-)
-plot(krig500["var1.pred"])
+# Krig 200 points ====
 
-krig_500pts <- ggplot2::ggplot() +
-  geom_sf(data = berlin_poly, alpha = 0.3) +
-  geom_sf(data = krig500, aes(fill = var1.pred), shape = 21, size = 3) +
-  geom_sf(data = pharos_500, aes(colour = detection_outcome), size = 2) + # foxes
-  scale_fill_viridis_c("probability", na.value = "white") +
-  scale_colour_manual("test outcome", values = c("#8a56b8", "#d5363d")) +
-  theme_void() +
-  coord_sf()
+## map 13 ====
+plot(krig_100pts)
 
-## map 3 ====
+# Krig 300 points ====
+
+## map 14 ====
 plot(krig_500pts)
 
-# With entropy ====
+# Krig 500 points ====
+
+## map 15 ====
+plot(krig_500pts)
+
+# Krig 1000 points ====
+
+## map 16 ====
+
+# Krig 2000 points ====
+
+## map 16 ====
+
+# Krig 5000 points ====
+
+## map 17 ====
+
+# Krig 10000 points ====
+
+## map 18 ====
+
+# Krig 25000 points ====
+
+## map 19 ====
+
 
 
 
--- src/julia/exporting_hotspots.jl
+++ src/julia/exporting_hotspots.jl
@@ -8,21 +8,61 @@ using DelimitedFiles
 file = ArchGDAL.read("C:\\Users\\abuss\\github\\urbanfox\\data\\clean\\krig_raster.tif")
 
 #raster band 
-    band = ArchGDAL.getband(file, 1) 
-    data = ArchGDAL.read(band) # Read as UInt32
-    data_matrix = Float64.(data) # Convert to Float64
+band = ArchGDAL.getband(file, 1) 
+data = ArchGDAL.read(band) # Read as UInt32
+data_matrix = Float64.(data) # Convert to Float64
 
 import Random
 Random.seed!(12345)
-    
+
 locations =
     data_matrix |> 
-    seed(BalancedAcceptance(; numpoints = 500)) |> 
-    refine(AdaptiveSpatial(; numpoints=400)) |>
+    seed(BalancedAcceptance(; numpoints = 1500)) |> 
+    refine(AdaptiveSpatial(; numpoints=1000)) |>
     first 
 
-# saving coordinates
-coord400 = [(location[1], location[2], data_matrix[location]) for location in locations]
-writedlm("coord400.txt", coord400)
+# saving coordinates for 1000 points
+coord1000 = [(location[1], location[2], data_matrix[location]) for location in locations]
+writedlm("coord1000.txt", coord1000)
 
-    
+locations =
+    data_matrix |> 
+    seed(BalancedAcceptance(; numpoints = 2500)) |> 
+    refine(AdaptiveSpatial(; numpoints=2000)) |>
+    first 
+
+# saving coordinates for 2000 points
+coord2000 = [(location[1], location[2], data_matrix[location]) for location in locations]
+writedlm("coord2000.txt", coord2000)
+
+locations =
+    data_matrix |> 
+    seed(BalancedAcceptance(; numpoints = 5500)) |> 
+    refine(AdaptiveSpatial(; numpoints=5000)) |>
+    first 
+
+# saving coordinates for 5000 points
+coord5000 = [(location[1], location[2], data_matrix[location]) for location in locations]
+writedlm("coord5000.txt", coord5000)
+
+locations =
+    data_matrix |> 
+    seed(BalancedAcceptance(; numpoints = 10500)) |> 
+    refine(AdaptiveSpatial(; numpoints=10000)) |>
+    first 
+
+# saving coordinates for 10 000 points
+coord10000 = [(location[1], location[2], data_matrix[location]) for location in locations]
+writedlm("coord10000.txt", coord10000)
+
+locations =
+    data_matrix |> 
+    seed(BalancedAcceptance(; numpoints = 25500)) |> 
+    refine(AdaptiveSpatial(; numpoints=25000)) |>
+    first 
+
+# saving coordinates for 25000
+coord25000 = [(location[1], location[2], data_matrix[location]) for location in locations]
+writedlm("coord25000.txt", coord25000)
+
+#please send me these .txt data of 1000, 2000, 5000, 10000, 25000 points :) Thanks!!!
